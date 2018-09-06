7503a1e6c268a1fb5428cb1d4b02a14c6b1c6839
Oscar <4991127@outlook.com>
2018-09-06 07:04:01 -0500

    pparently they do

---
+++ TEC/PROGRAMMING/009 Blackjack/Blackjack.java
@@ -0,0 +1,25 @@
+//Dealer must draw till 16 and stand on all 17s
+//
+//Game starts when you sit down
+//
+/*public final static void clearConsole()
+{
+    try
+    {
+        final String os = System.getProperty("os.name");
+
+        if (os.contains("Windows"))
+        {
+            Runtime.getRuntime().exec("cls");
+        }
+        else
+        {
+            Runtime.getRuntime().exec("clear");
+        }
+    }
+    catch (final Exception e)
+    {
+        //  Handle any exceptions.
+    }
+}
+*/
