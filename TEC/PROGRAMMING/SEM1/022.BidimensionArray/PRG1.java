7053b47005ddb534d7672ea5aee32b330c1c321c
Oscar <4991127@outlook.com>
2018-11-03 11:16:19 -0600

    Commmit message

--- TEC/PROGRAMMING/SEM1/022.BidimensionArray/PRG1.java
+++ TEC/PROGRAMMING/SEM1/022.BidimensionArray/PRG1.java
@@ -1,3 +1,34 @@
 //first the rows, tjhen the columns
 //the lenght of the multidimensional array will be the number of rows
 //using length on an element will output the columns 
+//
+//
+/*
+ *create bidimensional array
+ input columnsLIM
+ input rows
+ arr = makearray(columnsLIM, rows)
+    def makearray()
+        cols = make n arrays (cols) filled with randInt()
+        make x arrays (rows) using cols
+ print beautifyarray(arr)
+    def beautifyarray()
+        for i in range[rows[
+        print [ + row[i] + ]
+print menu()
+if option = switchcols
+    input(col1)
+    input(col2)
+
+else option = switch rows
+    input(row1)
+    input(row2)
+    back = arr[row1]
+    arr[row1]= arr[row2]
+    arr[row2]= back
+    print arr
+
+    
+else option = transpose
+else end
+ * */
