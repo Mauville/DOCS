9e80dc2210b4503f930aab706ec18de0e89adb65
Oscar <4991127@outlook.com>
2018-12-02 04:29:57 -0600

    Finished coffeegrade dependency handling

---
+++ TEC/PROGRAMMING/SEM1/023.StringMethods/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String str1 = "Oscar Vargas Perez";
+        int space = str1.indexOf(" ");
+        int name = str1.substring(0, space);
+
+    }
+}
